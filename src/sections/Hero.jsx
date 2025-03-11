396a6eb178f2f827233041d319f8afdb26a15f5d
Fabio <fabiomarinho202020@gmail.com>
2025-03-11 09:45:08 -0300

    ajuste layout

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,7 +2,7 @@ export default function Hero() {
   return (
     <section id="hero" className="relative h-screen flex items-center justify-center text-center text-white overflow-hidden">
       {/* Overlay com transparência */}
-      <div className="absolute inset-0 bg-black/30"></div>
+      <div className="absolute inset-0 bg-black/0"></div>
 
       {/* Conteúdo centralizado */}
       <div className="relative z-10 px-4 flex flex-col items-center">
